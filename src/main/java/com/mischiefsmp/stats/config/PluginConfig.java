e28d4ef072990288af9bcd1200f8ac5b6c05246f
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-07 18:29:09 +0200

    Create PluginConfig.java

---
+++ src/main/java/com/mischiefsmp/stats/config/PluginConfig.java
@@ -0,0 +1,25 @@
+package com.mischiefsmp.stats.config;
+
+import com.mischiefsmp.core.utils.FileUtils;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.util.FileUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PluginConfig {
+    private static FileConfiguration fc;
+
+    public static void init(Plugin plugin) {
+        fc = FileUtils.loadConfig(plugin, "config.yml");
+    }
+
+    public static String getDefaultLanguage() {
+        return fc.getString("language");
+    }
+
+    public static List<String> getLanguages() {
+        return fc.getStringList("languages");
+    }
+}
