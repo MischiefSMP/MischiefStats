c7ac933cbe590bc56c1e4a16e04314a24301d248
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-07 18:08:11 +0200

    Initial commit

---
+++ src/main/java/com/mischiefsmp/perms/MischiefStats.java
@@ -0,0 +1,11 @@
+package com.mischiefsmp.perms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MischiefStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
