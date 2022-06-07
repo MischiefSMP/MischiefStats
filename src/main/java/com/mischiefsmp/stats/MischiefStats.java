6fb14d35e9382719cbdbe8c9a2a6194c1e6fb541
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-07 18:16:22 +0200

    Reformat packages

--- src/main/java/com/mischiefsmp/stats/MischiefStats.java
+++ src/main/java/com/mischiefsmp/stats/MischiefStats.java
@@ -1,8 +1,10 @@
-package com.mischiefsmp.perms;
+package com.mischiefsmp.stats;
 
+import com.mischiefsmp.core.LangManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MischiefStats extends JavaPlugin {
+    private static LangManager langManager;
 
     @Override
     public void onEnable() {
