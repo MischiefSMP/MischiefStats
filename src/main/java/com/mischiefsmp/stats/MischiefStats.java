a0f2ec84dab34c226761eeb462148b9b3386d6d2
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-07 18:29:20 +0200

    Add boilerplate code

--- src/main/java/com/mischiefsmp/stats/MischiefStats.java
+++ src/main/java/com/mischiefsmp/stats/MischiefStats.java
@@ -1,13 +1,26 @@
 package com.mischiefsmp.stats;
 
 import com.mischiefsmp.core.LangManager;
+import com.mischiefsmp.core.LogManager;
+import com.mischiefsmp.stats.config.PluginConfig;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MischiefStats extends JavaPlugin {
+    private static LogManager logManager;
     private static LangManager langManager;
 
     @Override
     public void onEnable() {
+        PluginConfig.init(this);
+        logManager = new LogManager(this);
+        langManager = new LangManager(this, PluginConfig.getLanguages(), PluginConfig.getDefaultLanguage());
     }
 
+    public static LogManager getLogManager() {
+        return logManager;
+    }
+
+    public static LangManager getLangManager() {
+        return langManager;
+    }
 }
