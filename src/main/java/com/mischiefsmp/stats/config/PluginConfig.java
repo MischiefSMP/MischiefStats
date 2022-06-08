8fa25b412712fbdbf10a3ceb8070f282a7cb5332
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-08 13:01:54 +0200

    Small code cleanup

--- src/main/java/com/mischiefsmp/stats/config/PluginConfig.java
+++ src/main/java/com/mischiefsmp/stats/config/PluginConfig.java
@@ -7,25 +7,20 @@ import com.mischiefsmp.core.config.ConfigValue;
 import lombok.Getter;
 import org.bukkit.plugin.Plugin;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
+@Getter
 public class PluginConfig extends ConfigFile {
 
-    @Getter
     @ConfigValue(path = "language")
     private String defaultLanguage;
 
-    @Getter
     @ConfigValue(path = "languages")
     private ArrayList<String> languages;
 
-    @Getter
     @ConfigValue(path = "disabled-weapons")
     private ArrayList<String> disabledWeapons;
 
-    @Getter
     @ConfigValue(path = "disabled-causes")
     private ArrayList<String> disabledCauses;
 
