e50f06b5283ec2b416603eeeefcda0e27c90b1c9
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-08 14:57:29 +0200

    Finish playerstats class

--- src/main/java/com/mischiefsmp/stats/config/PlayerStats.java
+++ src/main/java/com/mischiefsmp/stats/config/PlayerStats.java
@@ -6,20 +6,81 @@ import com.mischiefsmp.core.config.ConfigValue;
 import lombok.Getter;
 import org.bukkit.plugin.Plugin;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Getter
 public class PlayerStats extends ConfigFile {
+    @ConfigValue(path = "playerKills")
+    private int playerKills;
+
+    @ConfigValue(path = "playerDeaths")
+    private int playerDeaths;
+
+    @ConfigValue(path = "deaths")
+    private int deaths;
 
     @ConfigValue(path = "suicides")
     private int suicides;
+
+    @ConfigValue(path = "totalDamage")
+    private double totalDamage;
+
+    @ConfigValue(path = "mostDamage")
+    private double mostDamage;
+
+    @ConfigValue(path = "killedMobs")
+    private Map<String, Integer> killedMobs = new HashMap<>();
+
+    @ConfigValue(path = "usedWeapons")
+    private Map<String, Integer> usedWeapons = new HashMap<>();
 
     public PlayerStats(Plugin plugin, UUID uuid) {
         super(plugin, String.format("players/%s.yml", uuid.toString()), "playertemplate.yml");
         ConfigManager.init(this);
     }
 
+    public void addPlayerKill() {
+        playerKills++;
+    }
+
+    public void addPlayerDeath() {
+        playerDeaths++;
+    }
+
+    public void addDeath() {
+        deaths++;
+    }
+
     public void addSuicide() {
         suicides++;
+    }
+
+    public void addTotalDamage(double damage) {
+        totalDamage += damage;
+    }
+
+    public void addMostDamage(double damage) {
+        if(damage > mostDamage)
+            mostDamage = damage;
+    }
+
+    public void addKilledMobs(String entity) {
+        if(killedMobs.containsKey(entity)) {
+            int current = killedMobs.get(entity) + 1;
+            killedMobs.put(entity, current);
+            return;
+        }
+        killedMobs.put(entity, 1);
+    }
+
+    public void addUsedWeapon(String weapon) {
+        if(usedWeapons.containsKey(weapon)) {
+            int current = usedWeapons.get(weapon) + 1;
+            usedWeapons.put(weapon, current);
+            return;
+        }
+        usedWeapons.put(weapon, 1);
     }
 }
