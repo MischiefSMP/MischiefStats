02597b08547bb1a2722c46305655e0d6284e7191
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-08 13:20:26 +0200

    Update PlayerStats.java

--- src/main/java/com/mischiefsmp/stats/config/PlayerStats.java
+++ src/main/java/com/mischiefsmp/stats/config/PlayerStats.java
@@ -1,4 +1,25 @@
 package com.mischiefsmp.stats.config;
 
-public class PlayerStats {
+import com.mischiefsmp.core.config.ConfigFile;
+import com.mischiefsmp.core.config.ConfigManager;
+import com.mischiefsmp.core.config.ConfigValue;
+import lombok.Getter;
+import org.bukkit.plugin.Plugin;
+
+import java.util.UUID;
+
+@Getter
+public class PlayerStats extends ConfigFile {
+
+    @ConfigValue(path = "suicides")
+    private int suicides;
+
+    public PlayerStats(Plugin plugin, UUID uuid) {
+        super(plugin, String.format("players/%s.yml", uuid.toString()), null);
+        ConfigManager.init(this);
+    }
+
+    public void addSuicide() {
+        suicides++;
+    }
 }
