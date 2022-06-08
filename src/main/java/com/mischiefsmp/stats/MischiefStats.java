c08295dc4cadd92b3a4716baad46814b579ba99b
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-08 13:00:53 +0200

    Reformat class

--- src/main/java/com/mischiefsmp/stats/MischiefStats.java
+++ src/main/java/com/mischiefsmp/stats/MischiefStats.java
@@ -2,6 +2,7 @@ package com.mischiefsmp.stats;
 
 import com.mischiefsmp.core.LangManager;
 import com.mischiefsmp.core.LogManager;
+import com.mischiefsmp.core.config.ConfigManager;
 import com.mischiefsmp.stats.config.PluginConfig;
 import com.mischiefsmp.stats.events.EntityDeath;
 import lombok.Getter;
@@ -10,10 +11,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class MischiefStats extends JavaPlugin {
     @Getter
     private static LogManager logManager;
-
     @Getter
     private static LangManager langManager;
-
     @Getter
     private static PluginConfig pluginConfig;
 
