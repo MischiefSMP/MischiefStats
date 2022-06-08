49cd168c1b1efcbf34da120f38b3499f446424fd
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-08 13:20:36 +0200

    Update PlayerStatsManager to save

--- src/main/java/com/mischiefsmp/stats/config/PlayerStatsManager.java
+++ src/main/java/com/mischiefsmp/stats/config/PlayerStatsManager.java
@@ -1,5 +1,6 @@
 package com.mischiefsmp.stats.config;
 
+import com.mischiefsmp.core.config.ConfigManager;
 import com.mischiefsmp.stats.MischiefStats;
 import com.mischiefsmp.stats.Utils;
 import org.bukkit.Bukkit;
@@ -11,11 +12,21 @@ import java.util.logging.Level;
 public class PlayerStatsManager {
     private static HashMap<UUID, PlayerStats> stats = new HashMap<>();
 
-    public static void addSuicideStat(UUID player) {
+    private static void ensurePlayerStat(UUID uuid) {
+        if(!stats.containsKey(uuid)) {
+            stats.put(uuid, new PlayerStats(MischiefStats.getInstance(), uuid));
+        }
+    }
 
+    public static void addSuicideStat(UUID uuid) {
+        ensurePlayerStat(uuid);
+        stats.get(uuid).addSuicide();
+        save();
     }
 
     public static void addKilledEntStat(UUID player, String entity, String cause, String weapon, double finalDamage) {
+        ensurePlayerStat(player);
+
         if(Utils.containsStringIgnoreCase(MischiefStats.getPluginConfig().getDisabledWeapons(), weapon))
             return;
         if(Utils.containsStringIgnoreCase(MischiefStats.getPluginConfig().getDisabledCauses(), cause))
@@ -24,5 +35,12 @@ public class PlayerStatsManager {
         //TODO: remove before release
         MischiefStats.getLogManager().logF("Adding stat for player %s, killed entity %s, cause %s with weapon %s", Level.INFO, Bukkit.getPlayer(player).getName(), entity, cause, weapon, finalDamage);
 
+    }
+
+    //TODO: Save every x minutes
+    public static void save() {
+        for(UUID uuid : stats.keySet()) {
+            ConfigManager.save(stats.get(uuid));
+        }
     }
 }
