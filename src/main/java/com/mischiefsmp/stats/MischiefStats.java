6e063aca354649bf100bd0cbfb0affa2dc3db600
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-09 09:03:32 +0200

    Utilize cmdinfo.yml

--- src/main/java/com/mischiefsmp/stats/MischiefStats.java
+++ src/main/java/com/mischiefsmp/stats/MischiefStats.java
@@ -2,12 +2,18 @@ package com.mischiefsmp.stats;
 
 import com.mischiefsmp.core.LangManager;
 import com.mischiefsmp.core.LogManager;
+import com.mischiefsmp.core.cmdinfo.CMDInfo;
+import com.mischiefsmp.core.cmdinfo.CMDInfoManager;
+import com.mischiefsmp.core.utils.MCUtils;
 import com.mischiefsmp.stats.config.PluginConfig;
 import com.mischiefsmp.stats.events.EntityDamage;
 import com.mischiefsmp.stats.events.EntityDeath;
 import com.mischiefsmp.stats.events.PlayerDeath;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.security.Permission;
+import java.util.HashMap;
 
 public class MischiefStats extends JavaPlugin {
     @Getter
@@ -18,6 +24,8 @@ public class MischiefStats extends JavaPlugin {
     private static LangManager langManager;
     @Getter
     private static PluginConfig pluginConfig;
+    @Getter
+    private static CMDInfoManager cmdInfoManager;
 
     @Override
     public void onEnable() {
@@ -25,9 +33,12 @@ public class MischiefStats extends JavaPlugin {
         pluginConfig = new PluginConfig(this);
         logManager = new LogManager(this);
         langManager = new LangManager(this, pluginConfig.getLanguages(), pluginConfig.getDefaultLanguage());
+        cmdInfoManager = new CMDInfoManager(this);
 
         getServer().getPluginManager().registerEvents(new EntityDeath(), this);
         getServer().getPluginManager().registerEvents(new PlayerDeath(), this);
         getServer().getPluginManager().registerEvents(new EntityDamage(), this);
+
+        MCUtils.registerPermissions(cmdInfoManager);
     }
 }
