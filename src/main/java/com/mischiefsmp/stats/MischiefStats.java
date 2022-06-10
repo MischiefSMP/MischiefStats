15dde12edc27fd1f38e5e2fc2b64d6f89b27fc68
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-10 09:23:07 +0200

    Add auto download of MischiefCore

--- src/main/java/com/mischiefsmp/stats/MischiefStats.java
+++ src/main/java/com/mischiefsmp/stats/MischiefStats.java
@@ -15,6 +15,13 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.logging.Level;
+
 public class MischiefStats extends JavaPlugin {
     @Getter
     private static MischiefStats instance;
@@ -27,8 +34,25 @@ public class MischiefStats extends JavaPlugin {
     @Getter
     private static CMDInfoManager cmdInfoManager;
 
+    public void ensureCore(boolean required) {
+        if(getServer().getPluginManager().getPlugin("MischiefCore") != null) return;
+        getLogger().log(Level.INFO, "Downloading MischiefCore...");
+        File pluginFile = new File(new File("").getAbsolutePath() + File.separator + "plugins" + File.separator + "MischiefCore.jar");
+        try {
+            InputStream in = new URL("https://github.com/MischiefSMP/MischiefCore/releases/latest/download/MischiefCore.jar").openStream();
+            Files.copy(in, pluginFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            getServer().getPluginManager().enablePlugin(getServer().getPluginManager().loadPlugin(pluginFile));
+        } catch (Exception e) {
+            getLogger().log(Level.SEVERE, "Error downloading MischiefCore! Message: " + e.getMessage());
+            if(required) getServer().shutdown();
+        }
+        getLogger().log(Level.INFO, "Done downloading MischiefCore!");
+    }
+
     @Override
     public void onEnable() {
+        ensureCore(true);
+
         instance = this;
         pluginConfig = new PluginConfig(this);
         logManager = new LogManager(this);
